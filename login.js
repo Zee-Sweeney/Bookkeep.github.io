8cd0fe57a9c476fc977cbd78675c0aa4314238e3
Zee-Sweeney <2839524@vikes.csuohio.edu>
2024-12-03 23:24:41 -0500

    12/3

---
+++ login.js
@@ -0,0 +1,14 @@
+function func(){
+    var email = document.getElementById("username").value;//just some js on top
+    var password = document.getElementById("password").value;
+
+    if(email === 'user1' && password === 'password123'){//hard coded for now
+        alert("Success!");
+        window.location.href = "budget.html"; // Redirects to budget.html
+        return false; // Prevents form from actually submitting
+    }
+    else{
+        alert("Invalid username or password");
+        return false; // Prevents form from actually submitting
+    }
+}
