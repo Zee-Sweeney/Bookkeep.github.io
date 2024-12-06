8cddc7891ba40f7a712f1fcdc2fe63c82ce931b6
Lili431 <2798122@vikes.csuohio.edu>
2024-12-05 20:39:23 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,14 +1,14 @@
-function login(){
-    var email = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
+function login() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
 
-    if(email === 'user1' && password === 'password123'){
-        alert("Success!");
+    if (username === 'user1' && password === 'password123') {
+        alert("Login successful!");
         window.location.href = "budget.html";
         return false;
-    }
-    else{
-        alert("Invalid username or password");
+    } else {
+        alert("Invalid username or password.");
         return false;
     }
 }
+
